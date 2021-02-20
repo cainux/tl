23f2e64b4afd25b8f6f1cfab70548976d179a943
Cain Voong <cainux@gmail.com>
2021-02-20 16:31:49 +0000

    Wire up FunTranslations. That rate limit is annoying.

--- src/TL.Pokedex.Infrastructure/TranslationServices/FunTranslationService.cs
+++ src/TL.Pokedex.Infrastructure/TranslationServices/FunTranslationService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using TL.Pokedex.Core.Abstractions.TranslationServices;
 
 namespace TL.Pokedex.Infrastructure.TranslationServices
@@ -17,9 +20,30 @@ namespace TL.Pokedex.Infrastructure.TranslationServices
             _logger = logger;
         }
 
-        public Task<string> GetTranslationAsync(string translator, string source)
+        public async Task<string> GetTranslationAsync(string translator, string source)
         {
-            throw new NotImplementedException();
+            var uri = new Uri($"{translator}", UriKind.Relative);
+            var formBody = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("text", source)
+            });
+
+            var responseMessage = await _httpClient.PostAsync(uri, formBody);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                dynamic translationResult = JsonConvert.DeserializeObject<JObject>(await responseMessage.Content.ReadAsStringAsync());
+
+                if ((int) translationResult.success.total > 0)
+                {
+                    var translation = (string) translationResult.contents.translated;
+                    return translation;
+                }
+            }
+
+            _logger.LogWarning("Failed to get translation, have you hit the rate limit?");
+
+            throw new TranslationException();
         }
     }
 }
